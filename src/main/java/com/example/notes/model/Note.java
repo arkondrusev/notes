2a35faadaf656960e876cec71cd340b8edb1974e
Artem <artem.kondrusev@gmail.com>
2024-07-03 03:04:42 +0300

    - Note model ORM mapping

--- src/main/java/com/example/notes/model/Note.java
+++ src/main/java/com/example/notes/model/Note.java
@@ -1,23 +1,37 @@
 package com.example.notes.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "note")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString(exclude = {"topic"})
 public class Note {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="note_id_seq")
+    @SequenceGenerator(name="note_id_seq", sequenceName="note_id_seq", allocationSize=1)
+    @Column(name = "id", updatable=false)
     @EqualsAndHashCode.Include
     @NonNull
     private Integer id;
     @NonNull
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
     private Topic topic;
     private String content;
+    @ManyToMany
+    @JoinTable(name = "note_to_tag",
+            joinColumns = @JoinColumn(name = "note_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
     private Set<Tag> tagList;
 
     public Note(Integer id, String name, Topic parentTopic, String content) {
